89431447943f19f9273d7c059d85c9fafbbbccec
Adrian Sims <asims@parkeon.com>
2017-10-09 17:59:29 +0100

    Add project files.

---
+++ gitmehappy.cmd
@@ -0,0 +1,18 @@
+rem echo off
+echo done >c:\temp\gitdone.txt
+git init DynTrigger1
+cd DynTrigger1
+git add DynTrigger1/App.xaml
+git add DynTrigger1/App.xaml.cs
+git add DynTrigger1/App.config
+git add DynTrigger1/MainWindow.xaml
+git add DynTrigger1/MainWindow.xaml.cs
+git add DynTrigger1/Properties/AssemblyInfo.cs
+git add DynTrigger1/app.config
+git add DynTrigger1/Properties/Settings.settings
+git add DynTrigger1/Properties/Resources.Designer.cs
+git add DynTrigger1/Properties/Resources.resx
+git add DynTrigger1/Properties/Settings.Designer.cs
+git add DynTrigger1/DynTrigger1.csproj
+git add DynTrigger1.sln
+git add .gitignore
